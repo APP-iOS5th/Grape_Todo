30e77a33c4bb25a6f8b8e8243b113ba87c383be0
YuleGlycerine <figurefig13@gmail.com>
2024-05-01 20:50:31 +0900

    Add Color extension

--- GrapeTodo/Model/SelectColor.swift
+++ GrapeTodo/Model/SelectColor.swift
@@ -7,17 +7,24 @@
 
 import SwiftUI
 
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet(charactersIn: "#"))
+        let rgbValue = UInt32(hex, radix: 16)
+        let r = Double((rgbValue! & 0xFF0000) >> 16) / 255
+        let g = Double((rgbValue! & 0x00FF00) >> 8) / 255
+        let b = Double(rgbValue! & 0x0000FF) / 255
+        self.init(red: r, green: g, blue: b)
+    }
+}
+
 enum SelectColor: String, CaseIterable {
-    case red, orange, yellow, green, blue, purple
+    case unchecked, checked
     
     var representable: Color {
         switch self {
-        case .red: .red
-        case .orange: .orange
-        case .yellow: .yellow
-        case .green: .green
-        case .blue: .blue
-        case .purple: .purple
+        case .unchecked: Color(Color(hex: "#F4F4F4"))
+        case .checked: Color(Color(hex: "#BAF7AD"))
         }
     }
 }
