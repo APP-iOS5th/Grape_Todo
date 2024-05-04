bb76178e68fdd3116ae5c8347ab290cfae3a8c39
changhyen yun <ychcom@naver.com>
2024-05-04 17:04:20 +0900

    로그인 기능 추가

---
+++ GrapeTodo/Model/Authentication.swift
@@ -0,0 +1,8 @@
+//
+//  Authentication.swift
+//  GrapeTodo
+//
+//  Created by changhyen yun on 5/3/24.
+//
+
+import Foundation
---
+++ GrapeTodo/View/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  GrapeTodo
+//
+//  Created by changhyen yun on 5/3/24.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
