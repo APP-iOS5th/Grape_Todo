22103853ea935766c3385eb8d0fa665015fd4888
changhyen yun <ychcom@naver.com>
2024-05-04 17:04:37 +0900

    로그인 기능 추가

--- GrapeTodo/View/LoginView.swift
+++ GrapeTodo/View/LoginView.swift
@@ -8,8 +8,57 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State 
+    private var email: String = ""
+    
+    @State
+    private var password: String = ""
+    
+    @EnvironmentObject
+    var authService: AuthService
+    
+    @Environment(\.dismiss)
+    var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            TextField("Email", text: $email).textFieldStyle(.roundedBorder)
+            SecureField("Password", text: $password).textFieldStyle(.roundedBorder)
+            Button("Login") {
+                authService.regularSignIn(email: email, password: password) { error in
+                    if let e = error {
+                        print(e.localizedDescription)
+                    }
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .controlSize(.large)
+            Button("Create an Account") {
+                authService.regularCreateAccount(email: email, password: password)
+            }
+            .buttonStyle(.borderedProminent)
+            .controlSize(.large)
+            Button {
+                print("Tapped apple sign in")
+                authService.startSignInWithAppleFlow()
+            } label: {
+                Image("AppleButton")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 300)
+            }
+            Button {
+                print("Tapped google sign in")
+                authService.googleSignIn()
+            } label: {
+                Image("GoogleButton")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 300)
+            }
+            
+            
+        }
     }
 }
 
